5023db92bd7216fa41c66953e56cabb6ee8bc45d
Weliton Silva <welitonsylva.2000@gmail.com>
2024-10-13 14:44:14 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -3,9 +3,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "recipes/home.html")
-
-def contato(request):
-    return render(request, "recipes/contato.html")
-
-def sobre(request):
-    return HttpResponse("Sobre")
