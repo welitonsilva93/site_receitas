5e028290e69827438d159b55cf36de08f523ae16
Weliton Silva <welitonsylva.2000@gmail.com>
2024-10-13 16:33:29 -0300

    iniciando site de receitas

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "recipes/home.html")
+    return render(request, "recipes/pages/home.html")
