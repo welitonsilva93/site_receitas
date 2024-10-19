c310f96f318256b3b44bef44b590d77d5c00df5a
Weliton Silva <welitonsylva.2000@gmail.com>
2024-10-19 14:28:38 -0300

    adicionando models ao projeto

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Rceipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipe/covers/%Y/%m/%d')
+    
