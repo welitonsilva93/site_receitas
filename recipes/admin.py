442af136c21893c1cca1c6d920d70a1997422711
Weliton Silva <welitonsylva.2000@gmail.com>
2024-10-19 15:55:52 -0300

    adicionando recipes ao admin e criando receitas no model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Recipe
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
 admin.site.register(Category, CategoryAdmin)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+    
