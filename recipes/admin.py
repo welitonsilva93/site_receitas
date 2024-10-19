c48b8b3da5d430365d6d6bf207b987503bbd585c
Weliton Silva <welitonsylva.2000@gmail.com>
2024-10-19 15:18:20 -0300

    adicionando categoria ao admin do django

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
