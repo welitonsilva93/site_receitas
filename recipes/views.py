26c251bd49a2545284758df03bf9298d1e1a1fc2
Weliton Silva <welitonsylva.2000@gmail.com>
2024-10-21 07:00:51 -0300

    adicionando erro 404 a categoria e mudando titulo da página

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
 from .models import Recipe
+from django.http import Http404
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -20,6 +21,10 @@ def category(request, category_id):
         category__id=category_id,
         is_published=True
     ).order_by('-id')
+
+    if not recipes:
+        raise Http404("Not found 🥲")
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category |',
     })
