6cfce81f775d011b1a7110668434f3878e9824d9
Weliton Silva <welitonsylva.2000@gmail.com>
2024-10-20 21:39:44 -0300

    corrigindo category com blank True e defaul none

--- recipes/models.py
+++ recipes/models.py
@@ -22,7 +22,7 @@ class Recipe(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(
         upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
