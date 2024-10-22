fd73e212c537f29de1f5dccd7a53bba40291438d
Weliton Silva <welitonsylva.2000@gmail.com>
2024-10-21 22:08:20 -0300

    Mostrando a página de detalhes da receita

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 from django.http import Http404
@@ -11,20 +10,21 @@ def home(request):
     })
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
+    recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id,
         is_published=True
-    ).order_by('-id')
-
-    if not recipes:
-        raise Http404("Not found 🥲")
+    ).order_by('-id'))
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category |',
+        'title': f'{recipes[0].category.name} - Category |',
     })
