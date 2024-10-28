b7fcac2111be22b6fc37f2181c50a4683486f171
Weliton Silva <welitonsylva.2000@gmail.com>
2024-10-28 06:38:25 -0300

    adicionando primeiros testes ao projeto

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class RecipeURLsTest(TestCase):
+    def test_recipe_url_is_correct(self):
+       home_url = reverse('recipes:home')
+       self.assertEqual(home_url, '/')
