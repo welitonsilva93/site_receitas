f360662646b2e81f2506a59e67a940b93b3f9601
Weliton Silva <welitonsylva.2000@gmail.com>
2024-11-01 18:34:55 -0300

    Criando uma classe de base para os testes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,10 +1,8 @@
-from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
-from recipes.models import Recipe, Category, User
+from .test_recipe_base import RecipeTestBase
 
-
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeTestBase):
 
    def test_recipe_home_view_fuction_is_correct(self):
       view = resolve(reverse('recipes:home'))
@@ -23,28 +21,6 @@ class RecipeViewsTest(TestCase):
       self.assertIn('<h1>No recipes found here 🥲</h1>', response.content.decode('utf-8'))
 
    def test_recipe_home_template_loads_recipes(self):
-      category = Category.objects.create(name='Category')
-      author = User.objects.create(
-         first_name='user',
-         last_name='name',
-         username='username',
-         password='123456',
-         email='username@email.com',
-      )
-      recipe = Recipe.objects.create(
-         category = category,
-         author = author,
-         title = 'Recipe Title',
-         description = 'Recipe Title',
-         slug = 'recipe-slug',
-         preparation_time = 10,
-         preparation_time_unit = 'Minutos',
-         servings = 5,
-         servings_unit = 'Porções',
-         preparation_steps = 'recipe preparation steps',
-         preparation_steps_is_html = False,
-         is_published = True,
-      )
    
       response = self.client.get(reverse('recipes:home'))
       content = response.content.decode('utf-8')
