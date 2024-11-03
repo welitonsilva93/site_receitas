a8598f74f154b5da1358ca6cf4de562d8f66879e
Weliton Silva <welitonsylva.2000@gmail.com>
2024-11-03 18:34:17 -0300

    retirando importação desnecessária e treinando falhas de testes proposital ou skip

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse, resolve
 from recipes import views
-from .test_recipe_base import RecipeTestBase, Recipe
+from .test_recipe_base import RecipeTestBase
 
 class RecipeViewsTest(RecipeTestBase):
 
